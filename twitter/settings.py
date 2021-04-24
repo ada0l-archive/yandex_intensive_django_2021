2aab61b7a48c7f5eca9028b5ddabe2b60913b6e5
ada0l <andreika.varfolomeev@yandex.ru>
2021-04-24 16:42:35 +1000

    fix

--- twitter/settings.py
+++ twitter/settings.py
@@ -147,3 +147,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
+
+if django_heroku:
+    django_heroku.settings(localls())
